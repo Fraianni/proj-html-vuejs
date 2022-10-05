eecf3fc2b1a9748139e93581069195dfabec5610
Fraianni <francescoianni1995@gmail.com>
2022-10-05 12:05:19 +0200

    header completo tranne icone social

--- src/components/HeroHeaderComponent.vue
+++ src/components/HeroHeaderComponent.vue
@@ -4,7 +4,9 @@
             <h5>I'M WINGMAN</h5>
             <h1>Peace On Earth A Wonderful Wish But No Way</h1>
             <p>Succes is not overnight.It is when everyday you get a little better than the day before</p>
+            <button> Get Started For &#8594;</button>
         </div>
+        <img src="../assets/motivation/images/home-movation-hero-image.png" alt="">
     </section>
   
 </template>
@@ -16,9 +18,29 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    .HeaderInfo{
-        max-width: 300px;
-        text-align:left;
+    section{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin-top: 100px;
+        .HeaderInfo{
+            max-width: 450px;
+            text-align:left;
+            font-size: 2rem;
+        p{
+            font-size: 1rem;
+        }
+        h5{
+            font-size: 1rem;
+        }
+        button{
+            border-radius: 5px;
+            border-style: none;
+            padding: 10px 50px;
+            color: white;
+            background-color: #20ac97;
+        }
+    }
     }
 
 </style>
