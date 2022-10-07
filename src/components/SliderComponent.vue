72f61c8225e7119a2fbe1a2392468e18af07f725
Fraianni <francescoianni1995@gmail.com>
2022-10-07 15:07:40 +0200

    miglioramento feedback section

--- src/components/SliderComponent.vue
+++ src/components/SliderComponent.vue
@@ -6,6 +6,8 @@
             <div class="active" v-if="slide.id === order">
                 <p>{{slide.description}}</p>
                 <img :src="require(`@/assets/motivation/images/${slide.thumb}`)" alt="">
+                <p>{{slide.name}}</p>
+                <p>{{slide.role}}</p>
             </div>
         </div>
         <div class="round-button">
@@ -56,27 +58,43 @@ export default {
 </script>
 
 <style scoped lang="scss">
+    @import '../style/variables.scss';
     .container{
         width: 100%;
         display: flex;
         .slides{
+            position: relative;
              width: 50%;
+             background-color: $athens_grey;
+             background-image: url('../assets/motivation/images/background-pattern-wavify.png');
             .round-button{
+                position: absolute;
+                right: 0px;
+                top: 45%;
+                transform: translateX(+50%);
                 background: red;
                 width: 100px;
                 height: 100px;
                 -moz-border-radius: 50px;
                 -webkit-border-radius: 50px;
                 border-radius: 50px;
-                background-color: red;
+                background-color: white;
                 display: flex;
                 flex-direction: column;
-                justify-content: center;
+                justify-content: space-around;
+                .prev,.next >*{
+                    color: gray;
+                }
             }
             .slide{
                 .active{
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: space-between;
                     img{
-                        width: 100%;
+                        border-radius: 50%;
+                        width: 150px;
                     }
                 }
             }
@@ -86,6 +104,7 @@ export default {
             width: 50%;
             img{
                 width: 100%;
+                height: 100%;
             }
         }
     }
